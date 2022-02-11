57f167802ff2cf64dee30da8a6d812824b6dffbb
yassine <yassinemekkor2002@gmail.com>
2022-02-11 15:55:19 +0100

    Creacion clase Rectangulo y obtenerArea

---
+++ src/main/java/Rectangulo.java
@@ -0,0 +1,15 @@
+public class Rectangulo {
+
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo(double lado1, double lado2){
+        this.lado1=lado1;
+        this.lado2=lado2;
+
+    }
+
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+}
